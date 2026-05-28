67bc54132d3c63ecf8ae889e388b912bbd1c1f6e
janadajayathilaka <lakvindujayathilaka@gmail.com>
2026-05-28 22:54:45 +0530

    feat: add user account component with dropdown menu and avatar display

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Sidebar,
   SidebarContent,
@@ -8,8 +9,11 @@ import {
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuItem,
+  SidebarMenuButton,
 } from "@/components/ui/sidebar"
 import { AudioLines, Mic, MicVocal } from "lucide-react"
+import { currentUser } from "@clerk/nextjs/server"
+import UserAccount from "./user-account"
 
 const routes = [
   {
@@ -31,7 +35,8 @@ const routes = [
     isBeta: true,
   },
 ]
-export function AppSidebar() {
+export async function AppSidebar() {
+  const user = await currentUser()
   return (
     <Sidebar>
       <SidebarHeader>
@@ -51,26 +56,43 @@ export function AppSidebar() {
                   key={route.title}
                   className="flex flex-1 rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 data-[state=open]:bg-gray-100"
                 >
-                  <a
-                    aria-disabled={route.isBeta}
-                    href={route.url}
-                    className="flex w-full items-center gap-2"
-                  >
-                    <route.icon className="h-4 w-4 shrink-0" />
-                    <span className="flex-1">{route.title}</span>
-                    {route.isBeta && (
-                      <span className="inline-flex items-center rounded-full border-transparent bg-gray-200 px-2 py-0.5 text-xs font-medium whitespace-nowrap text-black transition-colors">
-                        Beta
-                      </span>
-                    )}
-                  </a>
+                  <SidebarMenuButton asChild>
+                    <a
+                      aria-disabled={route.isBeta}
+                      href={route.url}
+                      className="flex w-full items-center gap-2"
+                    >
+                      <route.icon className="h-4 w-4 shrink-0" />
+                      <span className="flex-1">{route.title}</span>
+                      {route.isBeta && (
+                        <span className="inline-flex items-center rounded-full border-transparent bg-gray-200 px-2 py-0.5 text-xs font-medium whitespace-nowrap text-black transition-colors">
+                          Beta
+                        </span>
+                      )}
+                    </a>
+                  </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter></SidebarFooter>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>{/* <FreeCounter/> */}</SidebarMenuItem>
+          <SidebarMenuItem>
+            {user && (
+              <UserAccount
+                user={{
+                  name: user?.firstName || "User",
+                  email: user?.emailAddresses[0]?.emailAddress || "",
+                  avatar: user?.imageUrl,
+                }}
+              />
+            )}
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
---
+++ components/user-account.tsx
@@ -0,0 +1,80 @@
+import React from "react"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "./ui/sidebar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { ChevronDown, LogOut } from "lucide-react"
+import { useClerk } from "@clerk/nextjs"
+interface Props {
+  user: {
+    name: string
+    email: string
+    avatar: string
+  }
+}
+export default function UserAccount({ user }: Props) {
+  const { isMobile } = useSidebar()
+  const { signOut } = useClerk()
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="border-none outline-none focus-visible:ring-0 focus-visible:ring-transparent">
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarFallback className="rounded-lg">
+                  {user.name.charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+            <DropdownMenuContent
+              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              side={isMobile ? "bottom" : "right"}
+              align="end"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel>
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src={user.avatar} alt={user.name} />
+                    <AvatarFallback className="rounded-lg">
+                      {user.name.charAt(0)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">{user.name}</span>
+                    <span className="truncate text-xs">{user.email}</span>
+                  </div>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuItem onClick={() => signOut({ redirectUrl: "/" })}>
+                <LogOut />
+                Logout
+              </DropdownMenuItem>{" "}
+            </DropdownMenuContent>
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  )
+}
