d940332475e5c7000597e3a6163fe91d069d1439
janadajayathilaka <lakvindujayathilaka@gmail.com>
2026-06-09 22:50:08 +0530

    remove the logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,21 +1,23 @@
-import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import { FileHeart } from 'lucide-react';
-import Image from 'next/image';
-
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { FileHeart } from "lucide-react"
+import Image from "next/image"
 
 interface Props {
   href?: string
-  className?: string;
+  className?: string
 }
 
-export function Logo({ href = '/', className }: Props) {
+export function Logo({ href = "/", className }: Props) {
   return (
-    <Link href={href || "/"} className={cn("btn btn-ghost normal-case text-xl ml-4 flex items-center", className)}>
-      <Image width={32} height={32} className='border rounded' alt='logo' src={'/logo.png'} />
-      <h1 className={cn("text-2xl font-bold ml-2")}>
-        Text2SpeechAI
-      </h1>
+    <Link
+      href={href || "/"}
+      className={cn(
+        "btn btn-ghost ml-4 flex items-center text-xl normal-case",
+        className
+      )}
+    >
+      <h1 className={cn("ml-2 text-2xl font-bold")}>Text2SpeechAI</h1>
     </Link>
   )
 }
